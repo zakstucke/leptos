7a35f4cab436595d0630bcaee4954a94ef60b436
Greg Johnston <greg.johnston@gmail.com>
2024-02-10 17:17:45 -0500

    begin migrating to `leptos` and `leptos_dom` packages

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -16,7 +16,7 @@ pub fn SimpleCounter(
         <div>
             <button on:click=move |_| set_value(0)>"Clear"</button>
             <button on:click=move |_| set_value.update(|value| *value -= step)>"-1"</button>
-            <span>"Value: " {value} "!"</span>
+            <span>"Value: " {move || value.get()} "!"</span>
             <button on:click=move |_| set_value.update(|value| *value += step)>"+1"</button>
         </div>
     }
