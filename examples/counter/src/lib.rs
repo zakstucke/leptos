20e075f35d04f41400eb8359f5b59ba38fdf770c
Greg Johnston <greg.johnston@gmail.com>
2024-02-11 19:24:36 -0500

    working on examples

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -16,7 +16,7 @@ pub fn SimpleCounter(
         <div>
             <button on:click=move |_| set_value(0)>"Clear"</button>
             <button on:click=move |_| set_value.update(|value| *value -= step)>"-1"</button>
-            <span>"Value: " {move || value.get()} "!"</span>
+            <span>"Value: " {value} "!"</span>
             <button on:click=move |_| set_value.update(|value| *value += step)>"+1"</button>
         </div>
     }
